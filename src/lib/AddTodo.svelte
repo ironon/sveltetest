83528b7f3fb81b5648f5c4a357014c06be77284e
David Macpherson <david.allan.macpherson@gmail.com>
2022-12-24 10:04:58 -0500

    no way i made a basic todo app im so cool

---
+++ src/lib/AddTodo.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    export let todoAdded: (s:string) => void;
+    let value = ""
+    function addTodo() {
+        if (value == "") {
+            return
+        }
+        todoAdded(value)
+        value = ""
+    }
+    function onInput() {
+        value = this.value
+        
+    }
+    function keypress(e: KeyboardEvent) {
+        if (e.key == "Enter") {
+            value = this.value
+            addTodo()
+        }
+    }
+</script>
+
+<div id="addtodo">
+    <p>add ur todos or smth</p>
+    <input on:change={onInput} on:keydown={keypress} {value}>
+    <button on:click={addTodo}>ADD</button>
+</div>
+
+<style>
+    #addtodo {
+        
+    }
+</style>
---
+++ src/lib/ViewTodo.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    export let todos: string[]
+    function removeTodo(event, i:number) {
+        todos.splice(i, 1)
+        todos = todos
+    }
+</script>
+
+<div id="viewtodo">
+    {#each todos as todo, i}
+        <div id="todo">
+            <p>{todo}</p>
+            <button id="remove" on:click={(event) => removeTodo(event, i)}>X</button>
+        </div>
+    {/each}
+</div>
+
+<style>
+    #viewtodo {
+        border: 1px solid black;
+        margin-top: 1vh;
+        min-height: 10%;
+        padding-left: 1ch;
+    }
+    #todo {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between; 
+    }
+    #remove {
+        background-color: transparent;
+        border: 0px;
+        opacity: 0;
+        width: 30%;
+    }
+    #remove:hover {
+        background-color: transparent;
+        border: 0px;
+        opacity: 1;
+    }
+</style>
